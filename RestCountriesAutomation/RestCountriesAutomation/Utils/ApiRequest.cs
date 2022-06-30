0f437b7277f4282fe8686f09c12f680ddcf8e0de
Ankit <ankit@everwell.org>
2022-06-30 23:22:27 +0530

    initial commit

---
+++ RestCountriesAutomation/RestCountriesAutomation/Utils/ApiRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace RestCountriesAutomation.Utils
+{
+    class ApiRequest
+    {
+        public void sentGetRequest()
+        {
+        }
+    }
+}
