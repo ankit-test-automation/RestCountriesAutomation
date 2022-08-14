a2ffbc65d0e9c1372a0f881a7e2fceb422433eba
Ankit <ankitagrawal.wardha@gmail.com>
2022-08-14 22:05:11 +0530

    updated gitignore

--- src/test/java/org/ankit/ExampleTest.java
+++ src/test/java/org/ankit/ExampleTest.java
@@ -1,11 +1,28 @@
 package org.ankit;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ExampleTest {
 
     @Test
     public void test(){
+        Response response =
+                RestAssured.get("https://restcountries.com/v3.1/all");
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getStatusLine());
+        Assert.assertEquals(response.getStatusCode(),200);
+    }
 
+    @Test
+    public void test1(){
+        System.out.println("Test 2");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test 3");
     }
 }
