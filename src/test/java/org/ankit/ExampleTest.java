5ba697a2518e1d94995df78f9659779f00649f89
Ankit <ankitagrawal.wardha@gmail.com>
2022-08-15 17:42:50 +0530

    Added Json schema validator

--- src/test/java/org/ankit/ExampleTest.java
+++ src/test/java/org/ankit/ExampleTest.java
@@ -1,23 +1,27 @@
 package org.ankit;
 
-import static io.restassured.RestAssured.*;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
+import static io.restassured.RestAssured.*;
+
 public class ExampleTest {
 
     @Test
-    public void test(){
+    public void test() {
         Response response =
                 get("https://restcountries.com/v3.1/all");
         System.out.println(response.getStatusCode());
         System.out.println(response.getStatusLine());
-        Assert.assertEquals(response.getStatusCode(),200);
+        Assert.assertEquals(response.getStatusCode(), 200);
     }
 
     @Test
-    public void test1(){
+    public void test1() {
         baseURI = "https://restcountries.com/";
         given().
                 get("v3.1/all").
@@ -28,7 +32,15 @@ public class ExampleTest {
     }
 
     @Test
-    public void test2(){
-        System.out.println("Test 3");
+    public void test2() {
+
+        baseURI = "https://restcountries.com/";
+        given().
+                get("v2/alpha/col").
+                then().
+                assertThat().
+                body(JsonSchemaValidator.
+                        matchesJsonSchema(
+                                new File("resources/jsonschema.json")));
     }
 }
